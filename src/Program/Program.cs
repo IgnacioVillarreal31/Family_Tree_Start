e1ba664b33eb459abe480f57a6625295fba50f2a
IgnacioVillarreal31 <IGNACIO.VILLARREAL@correo.ucu.edu.uy>
2022-11-07 10:37:24 -0300

    Program cambiado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -25,6 +25,22 @@ namespace Program
             n3.AddChildren(n7);
 
             // visitar el árbol aquí
+            Persona p1 = new Persona("Gerardo",32);
+            Persona p2 = new Persona("Faker",12);
+            Persona p3 = new Persona("Leona",3);
+            Persona p4 = new Persona("Hasbulla",20);
+            Persona p5 = new Persona("Mr.Beast",38);
+            Persona p6 = new Persona("Santiago",18);
+            Persona p7 = new Persona("Mirtiña",1000000);
+            Persona p8 = new Persona("Jisus",1000000000);
+
+            p7.AddChildren(p2);
+            p7.AddChildren(p3);
+
+            p1.AddChildren(p4);
+            p1.AddChildren(p5);
+
+            p6.AddChildren(p8);
         }
     }
 }
